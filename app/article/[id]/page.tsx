f74428523d54025bded5bb3fa9fe00c8ceebdbd9
Shayan GeeDook <107814525+GeeDook@users.noreply.github.com>
2025-08-08 09:15:37 -0700

    Update page.tsx

--- app/article/[id]/page.tsx
+++ app/article/[id]/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import { ArrowLeft } from "lucide-react"
-import articles from "@/lib/data/articles.json";
+import articles from "lib/Data/articles.json";
 
 // This function is required for static site generation with dynamic routes
 export async function generateStaticParams() {
